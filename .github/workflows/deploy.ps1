5974d7842be5e762deba24cbec53dc74b5dc4e4c
Mert AKKAYA <rmertakkaya@gmail.com>
2021-04-07 05:41:57 +0300

    Update deploy.ps1

--- .github/workflows/deploy.ps1
+++ .github/workflows/deploy.ps1
@@ -1,3 +1,3 @@
-$Files = "c:\Users\meakkaya\RiderProjects\actions-runner\_\UdemyNLayerProject\UdemyNLayerProject\UdemyNLayerProject.API\outproject\*"
+$Files = "c:\Users\meakkaya\RiderProjects\actions-runner\_work\UdemyNLayerProject\UdemyNLayerProject\UdemyNLayerProject.API\outproject\*"
 $Destination = "\\ISTIT015\c$\AppHosts\runnerApp"
 copy-item -Path (Get-Item -Path "$Files").FullName -Include * -Destination "$Destination" -Force -Recurse
